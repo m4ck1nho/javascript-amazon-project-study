03ab4dbd36fa0ae9990758a28cb8d778a15f47d4
m4ck1nho <gkdnzkrks@gmail.com>
2025-11-27 21:01:23 +0300

    testing framework jasmine

---
+++ javascript-amazon-project-main/test-jasmine/moneyTest.js
@@ -0,0 +1,12 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+describe('test suite: formatCurrency', () =>{
+  it('converts cents into dolars',
+    () => {
+      expect(formatCurrency(100)).toEqual('$1.00');
+    });
+
+    it('works with 0', () => {
+      expect(formatCurrency(0).toEqual('0.00'));
+    });
+});
