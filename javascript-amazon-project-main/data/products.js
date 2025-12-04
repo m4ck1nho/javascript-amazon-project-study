084cb57b0b2cdb1a478387333da929c435c56eef
m4ck1nho <gkdnzkrks@gmail.com>
2025-12-04 15:41:50 +0300

    backend-setup

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -57,8 +57,28 @@ class Clothing extends Product{
   }
 }
 
+let products =[];
+
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+  if (productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
+  return new Product(productDetails);
+    });
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+
+}
 
 
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -724,4 +744,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
-
+*/
